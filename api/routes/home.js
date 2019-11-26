1faccd49a4e31a5dc0f529166ca61658b1d2bf18
Cody Swain <swain.cody@gmail.com>
2019-11-25 17:19:34 -0800

    Removed api subdirectory

--- api/routes/home.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/", function(require, res, next){
-    res.send("This message indicates that the backend is properly running");
-});
-
-module.exports = router; 
